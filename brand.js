e76e277f442013cea4f16fb0a646cba00d9e0310
Tahsin Sakin <tahcemdiscord@gmail.com>
2026-09-20 23:40:08 +0300

    Copy old belvia storage into budvia-v2

--- brand.js
+++ brand.js
@@ -1,1 +1,7 @@
+try{
+  if(!localStorage.getItem("budvia-v2")){
+    var old=localStorage.getItem("belvia-v2");
+    if(old) localStorage.setItem("budvia-v2", old);
+  }
+}catch(e){}
 window.BUDVIA_SHOT="data:image/jpeg;base64,"+(window._B1||"")+(window._B2||"");
